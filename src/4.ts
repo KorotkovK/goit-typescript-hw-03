a82233466b0c8413ba14034471412acc04b979f5
KorotkovK <korotkov.kostya123@gmail.com>
2023-12-03 02:09:29 +0200

    main

--- src/4.ts
+++ src/4.ts
@@ -1,69 +1,52 @@
 class Key {
-    private signature: number;
-  
-    constructor() {
-      this.signature = Math.random();
-    }
-  
-    getSignature(): number {
-      return this.signature;
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected key: Key;
+  protected tenants: object[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  abstract openDoor(key: Key): void;
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
     }
   }
-  
-  class Person {
-    private key: Key;
-  
-    constructor(key: Key) {
-      this.key = key;
-    }
-  
-    getKey(): Key {
-      return this.key;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
     }
   }
-  
-  abstract class House {
-    protected door: boolean;
-    protected key: Key;
-    protected tenants: Person[] = [];
-  
-    constructor(door: boolean, key: Key) {
-      this.door = door;
-      this.key = key;
-    }
-  
-    abstract openDoor(key: Key): void;
-  
-    comeIn(person: Person): void {
-      if (this.door) {
-        this.tenants.push(person);
-        console.log('Person entered the house.');
-      } else {
-        console.log('Door is closed. Person cannot enter.');
-      }
-    }
-  }
-  
-  class MyHouse extends House {
-    constructor(key: Key) {
-      super(true, key);
-    }
-  
-    openDoor(key: Key): void {
-      if (key.getSignature() === this.key.getSignature()) {
-        this.door = true;
-        console.log('Door opened.');
-      } else {
-        console.log('Incorrect key. Door remains closed.');
-      }
-    }
-  }
-  
-  // тестування
-  const key = new Key();
-  const house = new MyHouse(key);
-  const person = new Person(key);
-  
-  house.openDoor(person.getKey());
-  house.comeIn(person);
-  
+}
+
+const key = new Key();
+const person = new Person(key);
+const house = new MyHouse(key);
+
+house.openDoor(person.getKey());
+
+house.comeIn(person);
+
+export {};
